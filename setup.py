2a9a83437d096f56a365dea46cc454e59a9e1d93
Devon Jones <devon@knewton.com>
2012-03-28 18:36:42 -0400

    getting versioning working
    Change-Id: If94eff538e64e6459b15e64658b9a9a665eca1f8

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+def get_version():
+	v = file('version.txt', 'r')
+	with v:
+		lines = v.readlines()
+		return ''.join(lines).strip()
+
 setup(
 	name='k.config',
-	version='0.1', # TODO: replace on build, with build number.
+	version='0.1.%s' % get_version(),
 	url = 'https://wiki.knewton.net/index.php/Tech',
 	author='Devon Jones',
 	author_email='devon@knewton.com',
@@ -12,14 +18,6 @@ setup(
 	install_requires=[
 		'PyYAML>=3.09',
 	],
-	#tests_require=[
-	#	'cov-core>=1.3,<2.0',
-	#	'coverage>=3.5.1,<3.6',
-	#	'mock>=0.7.2,<0.8',
-	#	'py>=1.4.6,<1.5',
-	#	'pytest>=2.2.1,<2.3',
-	#	'pytest-cov>=1.5,<1.6',
-	#],
 	description = 'The knewton config library.',
 	long_description = '\n' + open('README').read(),
 )
