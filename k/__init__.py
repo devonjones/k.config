9146c98d82a5551f2984cf75f577b9dbf31d163f
Jonathan Kaczynski <jon@knewton.com>
2012-04-13 17:29:58 +0000

    filterwarnings
    Filtering out the "Module k was already imported from ..." UserWarning.
    
    Change-Id: I5c178bd4c2da56f251302aa2cc5c4410e6f5afd2

--- k/__init__.py
+++ k/__init__.py
@@ -1,3 +1,8 @@
+# This fix is so that you can reference modules in your local directory that
+# have k. as a parent while also referencing other modules that have k. as a
+# parent that are installed on your system/virtualenv
+import warnings
+warnings.filterwarnings('ignore', '.*Module k was already imported from.*', UserWarning)
 try:
 	__import__('pkg_resources').declare_namespace(__name__)
 except ImportError:
