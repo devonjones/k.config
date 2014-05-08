57e06458a75ff3996fb50b9448026695a443ec06
“Devon <devon.jones@sendgrid.com>
2014-05-08 16:59:00 -0600

    fixing requirements

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def parse_requirements(file_name):
 
 setup(
 	name="kconfig",
-	version="1.1.0",
+	version="1.1.1",
 	url = "https://github.com/Knewton/k.config",
 	author="Devon Jones",
 	author_email="devon.jones@gmail.com",
